f6a06ff3212ddf995c5b91b0db19ef9dc2333513
Phil Range <philrange@gmail.com>
2019-05-16 12:10:58 +0100

    added starsign icons

--- functions/index.js
+++ functions/index.js
@@ -17,6 +17,7 @@ var stylus = require('stylus');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var birthChartRouter = require('./routes/birthchart');
+var housesRouter = require('./routes/houses');
 const astronomia = require('astronomia')
 
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/birthchart', birthChartRouter);
+app.use('/houses', housesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
